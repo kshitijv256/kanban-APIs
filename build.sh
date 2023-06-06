dd6820019b3b23af62a78100bdc6bf90109f0cd1
Kshitij Verma <kshitijv250@gmail.com>
2023-06-06 17:22:39 +0530

    changed requirements file

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python /app/manage.py collectstatic --noinput
+
+gunicorn config.wsgi --bind 0.0.0.0:5000 --chdir=/app
+
 python manage.py migrate
